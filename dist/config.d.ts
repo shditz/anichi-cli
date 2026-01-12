3f950d8f583bfc6167c35d701c72a57364a37eeb
Aditya Kurniawan <99480729+shditz@users.noreply.github.com>
2026-01-12 23:46:37 +0700

    Delete dist directory

--- dist/config.d.ts
+++
@@ -1,6 +0,0 @@
-import { AppConfig } from "./types";
-export declare const ensureConfigDir: () => void;
-export declare const loadConfig: () => AppConfig;
-export declare const saveConfig: (config: AppConfig) => void;
-export declare const getConfigPath: () => string;
-//# sourceMappingURL=config.d.ts.map
--- dist/player.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const playUrl: (url: string, customPlayer?: string, customArgs?: string[], noBrowser?: boolean) => Promise<boolean>;
-//# sourceMappingURL=player.d.ts.map
